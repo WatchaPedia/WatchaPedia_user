52fcb339ceb3dbc6a67e3a14012b91000b34418a
hunsoep <gnstjq2772@naver.com>
2023-02-07 13:25:47 +0900

    book, detail 페이지 수정 완료

--- watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/model/network/response/content/BookResponse.java
+++ watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/model/network/response/content/BookResponse.java
@@ -8,28 +8,29 @@ import java.util.List;
 public record BookResponse(
         Long idx,String thumbnail,String title,String titleSub,
         String writer,String category,String atDate,String page,
-        String age,String summary,String people,String contentIdx,String pubSummary,
-        String backImg,String buy,List<Star> starList, double avgStar
+        String age,String summary,String people,String contentIdx,
+        String pubSummary, String makingDate,
+        String backImg,String bookBuy,List<Star> starList, double avgStar
 ){
     public static BookResponse of(
             Long idx, String thumbnail, String title, String titleSub,
             String writer, String category, String atDate,
             String page, String age, String summary, String people,
-            String contentIdx, String pubSummary, String backImg,
-            String buy,List<Star> starList
+            String contentIdx, String pubSummary,String makingDate, String backImg,
+            String bookBuy,List<Star> starList
     ){
 
         return new BookResponse(idx, thumbnail, title, titleSub,
                 writer, category, atDate, page, age, summary,
-                people, contentIdx, pubSummary, backImg, buy,starList,0.0);
+                people, contentIdx, pubSummary,makingDate, backImg, bookBuy,starList,0.0);
     }
 
     public static BookResponse of(
-            Long idx, String thumbnail, String title, String buy, Double avgStar
+            Long idx, String thumbnail, String title, String watch, Double avgStar
     ){
         return new BookResponse(
                 idx, thumbnail, title, null, null, null, null, null, null,
-                null, null, null, null , null, buy, null, avgStar
+                null, null, null, null , null, null, watch, null, avgStar
         );
     }
 
@@ -48,6 +49,7 @@ public record BookResponse(
                 dto.bookPeople(),
                 dto.bookContentIdx(),
                 dto.bookPubSummary(),
+                "",
                 dto.bookBackImg(),
                 dto.bookBuy(),
                 dto.starList(),
