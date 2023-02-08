4f086b52ffc337a74fee1761a4a932b13d332ca5
asg5450 <asg5450@gmail.com>
2023-02-08 11:25:04 +0900

    SearchBar push
    검색창 인기검색어, 최근검색어, 연관검색어 기능 구현

--- watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/model/dto/UserDto.java
+++ watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/model/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.watchapedia.watchpedia_user.model.dto;
 
 
+import com.watchapedia.watchpedia_user.model.entity.Search;
 import com.watchapedia.watchpedia_user.model.entity.User;
 import com.watchapedia.watchpedia_user.model.network.request.UserRequestDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record UserDto(
     Long userIdx,
@@ -22,8 +24,24 @@ public record UserDto(
     String userName,
     String userLikeActor,
     String userLikeDirector,
-    String userLikeGenre
+    String userLikeGenre,
+    List<Search> searchList
 ){
+    public static UserDto of(
+            Long userIdx, String userPw, Long userSsn1, Long userSsn2,
+            String userEmail, String userStatus, Long userCautionCnt, Long userWarningCnt,
+            Long userSuspensionCnt, LocalDateTime userLatelyStop, LocalDateTime userReleaseDate,
+            String userType, String userName, String userLikeActor, String userLikeDirector,
+            String userLikeGenre,List<Search> searchList
+    ){
+        return new UserDto(
+                userIdx, userPw, userSsn1, userSsn2, userEmail, userStatus,
+                userCautionCnt, userWarningCnt, userSuspensionCnt, userLatelyStop,
+                userReleaseDate, userType, userName, userLikeActor, userLikeDirector,
+                userLikeGenre,searchList
+        );
+    }
+
     public static UserDto of(
             Long userIdx, String userPw, Long userSsn1, Long userSsn2,
             String userEmail, String userStatus, Long userCautionCnt, Long userWarningCnt,
@@ -35,7 +53,7 @@ public record UserDto(
                 userIdx, userPw, userSsn1, userSsn2, userEmail, userStatus,
                 userCautionCnt, userWarningCnt, userSuspensionCnt, userLatelyStop,
                 userReleaseDate, userType, userName, userLikeActor, userLikeDirector,
-                userLikeGenre
+                userLikeGenre, null
         );
     }
 
@@ -46,7 +64,7 @@ public record UserDto(
         return new UserDto(
                 null, userPw, userSsn1, userSsn2, userEmail, null,
                 null, null, null, null, null, null, userName, null, null,
-                null
+                null, null
         );
     }
 
@@ -67,7 +85,8 @@ public record UserDto(
                 entity.getUserName(),
                 entity.getUserLikeActor(),
                 entity.getUserLikeDirector(),
-                entity.getUserLikeGenre()
+                entity.getUserLikeGenre(),
+                entity.getSearchList()
         );
     }
     public static UserDto requestReceive(UserRequestDto userRequestdto){
@@ -85,6 +104,7 @@ public record UserDto(
                 null,
                 "일반유저",
                 userRequestdto.userName(),
+                null,
                 null,
                 null,
                 null
