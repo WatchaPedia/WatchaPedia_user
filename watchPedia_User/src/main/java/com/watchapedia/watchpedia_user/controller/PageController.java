541c0ffe3206c1890601951971e18f79d1a68d75
hunsoep <gnstjq2772@naver.com>
2023-02-09 14:27:56 +0900

    최근 등록된 페이지 리스트 출력

--- watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/controller/PageController.java
+++ watchPedia_User/src/main/java/com/watchapedia/watchpedia_user/controller/PageController.java
@@ -53,6 +53,7 @@ public class PageController {
         map.addAttribute("userSession", userSessionDto);
         List<MovieDto> movies = movieService.movies();
         map.addAttribute("movies", movies);
+        map.addAttribute("movieDtos", movieService.movieDtos());
         map.addAttribute("movies2", movieService.movies2("나 홀로"));
         map.addAttribute("Irons", movieService.Irons("아이언"));
         map.addAttribute("movies3", movieService.movies3("2023"));
